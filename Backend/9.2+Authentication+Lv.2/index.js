3759b140013ac35d124d993fad437d0be66f0e37
ToniDevStyle <tvr9869@gmail.com>
2024-05-04 18:19:33 +0200

    added coments

--- Backend/9.2+Authentication+Lv.2/index.js
+++ Backend/9.2+Authentication+Lv.2/index.js
@@ -6,6 +6,7 @@ import e from "express";
 
 const app = express();
 const port = 3000;
+//  Number of salt rounds for password hashing
 const saltRounds = 10;
 
 const db = new pg.Client({
@@ -36,18 +37,22 @@ app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
 
+  // Checking if the email already exists in the database
   try {
     const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
       email,
     ]);
 
     if (checkResult.rows.length > 0) {
+      // Sending response if email already exists
       res.send("Email already exists. Try logging in.");
     } else {
+      // Hashing the password
       bcrypt.hash(password, saltRounds, async (err, hash)=> {
         if(err){
             console.log(err);
         }else {
+          // Inserting user data into the database
         const result = await db.query(
           "INSERT INTO users (email, password) VALUES ($1, $2)",
           [email, hash]
@@ -68,13 +73,16 @@ app.post("/login", async (req, res) => {
   const password = req.body.password;
 
   try {
+    // Retrieving user data from the database based on email
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
       email,
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
+      // Retrieving stored password from database
       const storedPassword = user.password;
 
+      // Comparing provided password with stored hashed password
       bcrypt.compare(password, storedPassword, (err, result) =>{
         if(err){
           console.log(err);
